9d54cb7d140c8c8a41d39910aaec76a103b5bcbe
Matthew Fernando Anggrian <matthewanggrian72@gmail.com>
2026-05-11 09:40:18 +0700

    Initial Commit

---
+++ Ploong/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Ploong
+//
+//  Created by Matthew Fernando Anggrian on 11/05/26.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}

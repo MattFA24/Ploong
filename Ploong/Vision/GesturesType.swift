3caaa50b41625470017cf598f97a3dafd67e4794
joycelyneb2 <joycelynbusiness1@gmail.com>
2026-05-15 08:56:18 +0700

    FEAT: add hand gesture on calibration scene and gameplay
    +fix retry

--- Ploong/Vision/GesturesType.swift
+++ Ploong/Vision/GesturesType.swift
@@ -3,5 +3,24 @@
 //  Ploong
 //
 //  Created by Matthew Fernando Anggrian on 11/05/26.
-//https://gemini.google.com/app
+//
 
+enum HandGesture: Equatable, Sendable {
+    case fist
+    case point
+    case unrecognized
+    case unknown
+
+    var displayText: String {
+        switch self {
+        case .fist:
+            return "Gesture: Fist"
+        case .point:
+            return "Gesture: Point"
+        case .unrecognized:
+            return "Hand detected"
+        case .unknown:
+            return "No hand detected"
+        }
+    }
+}
